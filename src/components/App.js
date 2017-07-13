1569a5d5e3b011118f60fe1d1cf69d47e06363e3
Alin Porumb <alin.porumb@gmail.com>
2017-07-13 20:19:01 +0300

    Step 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import ProductList from "./ProductList";
+import {getAll} from "../api/products";
 
-const App = (props) => {
-  return (
-    <div>
+class App extends React.Component {
+  state = {
+    products : []
+  }
+
+  componentDidMount() {
+    getAll().then(products => {
+      this.setState({products});
+    })
+  }
+
+  render() {
+    console.log(this.state);
+
+    return <div>
       App..
-    </div>
-  );
-};
+
+      <hr/>
+
+      <ProductList products={this.state.products}/>
+    </div>;
+  }
+}
 
 export default App;
